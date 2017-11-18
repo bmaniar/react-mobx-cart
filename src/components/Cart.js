551535b0ed8fcde46d0ddf8536bcf57140765a28
Bhargav Maniar 2 <bmani1@sapient.com>
2017-11-18 14:30:17 +0530

    Cart component clean up

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { inject, observer} from 'mobx-react';
-
+import CartItem from './CartItem';
 @inject('cartStore')
 @observer
 export default class Cart extends React.Component {
@@ -9,29 +9,21 @@ export default class Cart extends React.Component {
         this.state = {
           items: []
         }
-        this.handleClick = this.handleClick.bind(this);
     }
-    ComponentWillMount(){
+    componentWillMount(){
         var items = this.props.cartStore.fetchCart();
         this.setState({ items: items });
     }
-    handleClick = e => {
-        this.props.cartStore.removeFromCart(e);    
-    } 
-    renderTodos() {
-        return this.state.items.map((item, idx) => {
-            return (<li  key = {idx} >
-                {item.name} 
-                <button onClick = {() => this.handleClick(item)} > Add to Cart </button>
-            </li>)
-        })
-    } 
     render() {
         return (
         <div>
             <h2>Cart</h2>
             <ul>
-                {this.renderTodos()}
+               {
+                   this.state.items.map((item, idx) => {
+                    return (<CartItem item={item} key={idx}/>)
+                })
+               }
             </ul>
         </div>
         );

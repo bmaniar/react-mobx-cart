ee82a2210bbd5903a506a7e3a7c8cfa56a44a16f
Bhargav Maniar 2 <bmani1@sapient.com>
2017-11-18 17:12:22 +0530

    App observer added

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Switch, Route} from 'react-router-dom';
 import DevTools from 'mobx-react-devtools';
 import Home from './Home';
-
+import {observer} from 'mobx-react';
+@observer
 export default class App extends React.Component {
   render() {
       return (
